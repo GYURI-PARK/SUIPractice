ce0ad2c26808ac50f4b9351236cc9340c8ffc58c
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-03-23 15:50:58 +0900

    Initial Commit

---
+++ SUIPractice/SUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIPracticeApp.swift
+//  SUIPractice
+//
+//  Created by GYURI PARK on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
