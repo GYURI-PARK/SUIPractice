6a9a0f429c7a62ccdbdf3c7bbea77949aba20166
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-03-24 18:36:20 +0900

    add first image

--- SUIPractice/ContentView.swift
+++ SUIPractice/ContentView.swift
@@ -14,50 +14,195 @@ struct ContentView: View {
     
     var body: some View {
         
-        ScrollView {
-            ZStack{
-                VStack{
-                    Image("london_1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 300)
-                        .cornerRadius(15)
-                    
-                    HStack {
-                        Text("London, United Kingdom")
-                        
-                        Button {
-                            isFilling.toggle()
-                        } label: {
-                            Text("SAVE")
-                                .padding(10)
-                                .foregroundColor(.red)
-                                .background(Color("DorisColor"))
-                                .cornerRadius(10)
-                        }
-                    }
+        List {
+            Section {
+                HStack {
+                    Image(systemName: "heart")
+                    Text("Asia")
                 }
             }
             
-            ZStack{
-                VStack{
-                    Image("paris_1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 300)
-                        .cornerRadius(15)
-                    
-                    HStack {
-                        Text("Paris, France")
+            Section {
+                HStack {
+                    Image(systemName: "heart")
+                    Text("America")
+                }
+            }
+            
+            
+            Section {
+                HStack {
+                    Image(systemName: "heart")
+                    Text("Europe")
+                }
+            }
+            
+            ScrollView {
+                
+                ZStack{
+                    VStack{
+                        Image("london_1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(15)
                         
-                        Button {
-                            isFilling.toggle()
-                        } label: {
-                            Text("SAVE")
-                                .padding(10)
-                                .foregroundColor(.red)
-                                .background(Color("DorisColor"))
-                                .cornerRadius(10)
+                        HStack {
+                            Text("London, United Kingdom")
+                            
+                            Button {
+                                isFilling.toggle()
+                            } label: {
+                                Text("SAVE")
+                                    .padding(10)
+                                    .foregroundColor(.red)
+                                    .background(Color("DorisColor"))
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                
+                ZStack{
+                    VStack{
+                        Image("paris_1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(15)
+                        
+                        HStack {
+                            Text("Paris, France")
+                            
+                            Button {
+                                isFilling.toggle()
+                            } label: {
+                                Text("SAVE")
+                                    .padding(10)
+                                    .foregroundColor(.red)
+                                    .background(Color("DorisColor"))
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                
+                ZStack{
+                    VStack{
+                        Image("praha_1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(15)
+                        
+                        HStack {
+                            Text("Praha, Czech Republic")
+                            
+                            Button {
+                                isFilling.toggle()
+                            } label: {
+                                Text("SAVE")
+                                    .padding(10)
+                                    .foregroundColor(.green)
+                                    .background(Color("DorisColor"))
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                
+                ZStack{
+                    VStack{
+                        Image("buda_1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(15)
+                        
+                        HStack {
+                            Text("Budapest, Hungary")
+                            
+                            Button {
+                                isFilling.toggle()
+                            } label: {
+                                Text("SAVE")
+                                    .padding(10)
+                                    .foregroundColor(.green)
+                                    .background(Color("DorisColor"))
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                
+                ZStack{
+                    VStack{
+                        Image("sweden_1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(15)
+                        
+                        HStack {
+                            Text("Stockholm, Sweden")
+                            
+                            Button {
+                                isFilling.toggle()
+                            } label: {
+                                Text("SAVE")
+                                    .padding(10)
+                                    .foregroundColor(.green)
+                                    .background(Color("DorisColor"))
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                
+                ZStack{
+                    VStack{
+                        Image("kiruna_1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(15)
+                        
+                        HStack {
+                            Text("Kiruna, Sweden")
+                            
+                            Button {
+                                isFilling.toggle()
+                            } label: {
+                                Text("SAVE")
+                                    .padding(10)
+                                    .foregroundColor(.green)
+                                    .background(Color("DorisColor"))
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                
+                ZStack{
+                    VStack{
+                        Image("berlin_1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(15)
+                        
+                        HStack {
+                            Text("Berlin, Germany")
+                            
+                            Button {
+                                isFilling.toggle()
+                            } label: {
+                                Text("SAVE")
+                                    .padding(10)
+                                    .foregroundColor(.green)
+                                    .background(Color("DorisColor"))
+                                    .cornerRadius(10)
+                            }
                         }
                     }
                 }
