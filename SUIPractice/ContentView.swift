e502f8811c120f7ae61091d997ac3087255f1f63
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2023-03-26 03:25:06 +0900

    group 컴포넌트 사용
    하나의 뷰안에 10개이상 데이터 들어가면 에러남

--- SUIPractice/ContentView.swift
+++ SUIPractice/ContentView.swift
@@ -21,15 +21,12 @@ struct ContentView: View {
                     Text("Asia")
                 }
             }
-            
             Section {
                 HStack {
                     Image(systemName: "heart")
                     Text("America")
                 }
             }
-            
-            
             Section {
                 HStack {
                     Image(systemName: "heart")
@@ -39,179 +36,199 @@ struct ContentView: View {
             
             ScrollView {
                 
-                ZStack{
-                    VStack{
-                        
-                        ScrollView(.horizontal) {
-                            HStack{
-                                Image("london_1")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 300)
-                                    .cornerRadius(15)
-                                Image("praha_1")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 300)
-                                    .cornerRadius(15)
+                Group{
+                    ZStack{
+                        VStack{
+                            
+                            ScrollView(.horizontal) {
+                                HStack{
+                                    Image("london_1")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 300)
+                                        .cornerRadius(15)
+                                    Image("praha_1")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 300)
+                                        .cornerRadius(15)
+                                }
+                            }
+                            HStack {
+                                Text("London, United Kingdom")
+                                
+                                Button {
+                                    isFilling.toggle()
+                                } label: {
+                                    Text("SAVE")
+                                        .padding(5)
+                                        .foregroundColor(.green)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                }
                             }
                         }
-                        HStack {
-                            Text("London, United Kingdom")
+                    }
+                    
+                    ZStack{
+                        VStack{
+                            Image("paris_1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300)
+                                .cornerRadius(15)
                             
-                            Button {
-                                isFilling.toggle()
-                            } label: {
-                                Text("SAVE")
-                                    .padding(5)
-                                    .foregroundColor(.green)
-                                    .background(.black)
-                                    .cornerRadius(10)
+                            HStack {
+                                Text("Paris, France")
+                                
+                                Button {
+                                    isFilling.toggle()
+                                } label: {
+                                    Text("SAVE")
+                                        .padding(5)
+                                        .foregroundColor(.green)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                }
+                            }
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(height: 25)
+                    
+                    ZStack{
+                        VStack{
+                            Image("praha_1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 300)
+                                .cornerRadius(15)
+                                .clipped()
+                            
+                            HStack {
+                                Text("Praha, Czech Republic")
+                                
+                                Button {
+                                    isFilling.toggle()
+                                } label: {
+                                    Text("SAVE")
+                                        .padding(5)
+                                        .foregroundColor(.green)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                }
+                            }
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(height: 25)
+                    
+                    ZStack{
+                        VStack{
+                            Image("buda_1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300)
+                                .cornerRadius(15)
+                            
+                            HStack {
+                                Text("Budapest, Hungary")
+                                
+                                Button {
+                                    isFilling.toggle()
+                                } label: {
+                                    Text("SAVE")
+                                        .padding(5)
+                                        .foregroundColor(.green)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                }
                             }
                         }
                     }
                 }
                 
-                ZStack{
-                    VStack{
-                        Image("paris_1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300)
-                            .cornerRadius(15)
-                        
-                        HStack {
-                            Text("Paris, France")
+                
+                Spacer()
+                    .frame(height: 25)
+                
+                Group{
+                    ZStack{
+                        VStack{
+                            Image("sweden_1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300)
+                                .cornerRadius(15)
                             
-                            Button {
-                                isFilling.toggle()
-                            } label: {
-                                Text("SAVE")
-                                    .padding(5)
-                                    .foregroundColor(.green)
-                                    .background(.black)
-                                    .cornerRadius(10)
+                            HStack {
+                                Text("Stockholm, Sweden")
+                                
+                                Button {
+                                    isFilling.toggle()
+                                } label: {
+                                    Text("SAVE")
+                                        .padding(5)
+                                        .foregroundColor(.green)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                }
                             }
                         }
                     }
-                }
-                
-                ZStack{
-                    VStack{
-                        Image("praha_1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 300)
-                            .cornerRadius(15)
-                            .clipped()
-                        
-                        HStack {
-                            Text("Praha, Czech Republic")
+                    
+                    Spacer()
+                        .frame(height: 25)
+                    
+                    ZStack{
+                        VStack{
+                            Image("kiruna_1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300)
+                                .cornerRadius(15)
                             
-                            Button {
-                                isFilling.toggle()
-                            } label: {
-                                Text("SAVE")
-                                    .padding(5)
-                                    .foregroundColor(.green)
-                                    .background(.black)
-                                    .cornerRadius(10)
+                            HStack {
+                                Text("Kiruna, Sweden")
+                                
+                                Button {
+                                    isFilling.toggle()
+                                } label: {
+                                    Text("SAVE")
+                                        .padding(5)
+                                        .foregroundColor(.green)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                }
                             }
                         }
                     }
-                }
-                
-                ZStack{
-                    VStack{
-                        Image("buda_1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300)
-                            .cornerRadius(15)
-                        
-                        HStack {
-                            Text("Budapest, Hungary")
+                    
+                    Spacer()
+                        .frame(height: 25)
+                    
+                    ZStack{
+                        VStack{
+                            Image("berlin_1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300)
+                                .cornerRadius(15)
                             
-                            Button {
-                                isFilling.toggle()
-                            } label: {
-                                Text("SAVE")
-                                    .padding(5)
-                                    .foregroundColor(.green)
-                                    .background(.black)
-                                    .cornerRadius(10)
-                            }
-                        }
-                    }
-                }
-                
-                ZStack{
-                    VStack{
-                        Image("sweden_1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300)
-                            .cornerRadius(15)
-                        
-                        HStack {
-                            Text("Stockholm, Sweden")
-                            
-                            Button {
-                                isFilling.toggle()
-                            } label: {
-                                Text("SAVE")
-                                    .padding(5)
-                                    .foregroundColor(.green)
-                                    .background(.black)
-                                    .cornerRadius(10)
-                            }
-                        }
-                    }
-                }
-                
-                ZStack{
-                    VStack{
-                        Image("kiruna_1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300)
-                            .cornerRadius(15)
-                        
-                        HStack {
-                            Text("Kiruna, Sweden")
-                            
-                            Button {
-                                isFilling.toggle()
-                            } label: {
-                                Text("SAVE")
-                                    .padding(5)
-                                    .foregroundColor(.green)
-                                    .background(.black)
-                                    .cornerRadius(10)
-                            }
-                        }
-                    }
-                }
-                
-                ZStack{
-                    VStack{
-                        Image("berlin_1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300)
-                            .cornerRadius(15)
-                        
-                        HStack {
-                            Text("Berlin, Germany")
-                            
-                            Button {
-                                isFilling.toggle()
-                            } label: {
-                                Text("SAVE")
-                                    .padding(5)
-                                    .foregroundColor(.green)
-                                    .background(.black)
-                                    .cornerRadius(10)
+                            HStack {
+                                Text("Berlin, Germany")
+                                
+                                Button {
+                                    isFilling.toggle()
+                                } label: {
+                                    Text("SAVE")
+                                        .padding(5)
+                                        .foregroundColor(.green)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                }
                             }
                         }
                     }
