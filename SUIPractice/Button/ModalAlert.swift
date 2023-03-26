431702dc3722b3adb7104cdf3632538827bb1681
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-03-26 12:32:27 +0900

    Create ModalAlert.swift

---
+++ SUIPractice/Button/ModalAlert.swift
@@ -0,0 +1,48 @@
+//
+//  ModalAlert.swift
+//  SUIPractice
+//
+//  Created by GYURI PARK on 2023/03/26.
+//
+
+import SwiftUI
+
+struct ModalAlert: View {
+    
+    @State var isShowingModal: Bool = false
+    @State var isShowingAlert: Bool = false
+    
+    var body: some View {
+        
+        VStack{
+            Button{
+                isShowingModal = true
+            } label: {
+                Text("Show Country")
+            }
+            .sheet(isPresented: $isShowingModal) {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Modal view")
+                }
+            }
+            .padding()
+            
+            Button{
+                isShowingAlert = true
+            } label: {
+                Text("Alert test")
+            }
+            .alert(isPresented: $isShowingAlert) {
+                Alert(title: Text("this is alert"), primaryButton: .cancel(), secondaryButton: .default((Text("OK"))))
+            }
+            
+        }
+    }
+}
+
+struct ModalAlert_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalAlert()
+    }
+}
