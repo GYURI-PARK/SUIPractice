64fcb98dd1d94ab50435b1ab185e422a9d63a826
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-03-26 21:55:36 +0900

    ProgressView

---
+++ SUIPractice/View/NewProgressView.swift
@@ -0,0 +1,42 @@
+//
+//  NewProgressView.swift
+//  SUIPractice
+//
+//  Created by GYURI PARK on 2023/03/26.
+//
+
+import SwiftUI
+
+struct NewProgressView: View {
+    
+    @State var progress: Double = 0
+    @State var num: Int = 0
+    
+    var body: some View {
+        VStack{
+            ProgressView("Question \(num) / 8", value: progress, total: 80)
+            
+            HStack{
+                Button {
+                    progress -= 10
+                    num -= 1
+                } label: {
+                    Text("Back")
+                }
+                
+                Button {
+                    progress += 10
+                    num += 1
+                } label: {
+                    Text("Next")
+                }
+            }
+        }
+    }
+}
+
+struct NewProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewProgressView()
+    }
+}
