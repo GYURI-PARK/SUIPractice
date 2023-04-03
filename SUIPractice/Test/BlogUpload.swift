466a80b422ee6b6a17fd8f616ccf05acc6f3183d
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-04-03 14:48:22 +0900

    blog update

--- SUIPractice/Test/BlogUpload.swift
+++ SUIPractice/Test/BlogUpload.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct BlogUpload: View {
-    var str = "A"
+    @State var num : Int = 160
+    @State var isShowingModal: Bool = false
     
     var body: some View {
         VStack{
-            Text(str)
-            Button("Change") {
-                str = "B"
+            Text("Now, the number is \(num)")
+                .font(.system(size: 30))
+                .bold()
+                .padding(.bottom, 40)
+            
+            Button(action:
+                    {self.num += 1},
+                   label: {Text("Add number")
+                    .frame(width: 200, height: 50)
+                    .background(Color.black)
+                    .cornerRadius(15)
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+            })
+            .padding(.bottom, 40)
+            
+            Button(action:
+                    {isShowingModal = true},
+                   label: {Text("Show Modal")
+                    .frame(width: 200, height: 50)
+                    .background()
+                    .shadow(radius: 10)
+                    .foregroundColor(.black)
+            }) .sheet(isPresented: $isShowingModal){
+                ZStack{
+                    Color.black.ignoresSafeArea()
+                    Text("Check the number : \(num)")
+                        .font(.system(size: 30))
+                        .foregroundColor(.white)
+                    
+                }
             }
+            
         }
     }
 }
+
 
 struct BlogUpload_Previews: PreviewProvider {
     static var previews: some View {
