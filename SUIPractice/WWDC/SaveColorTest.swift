d16036e51becee36e41465b7b10a0939197f4edc
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-04-13 20:46:31 +0900

    colorList

---
+++ SUIPractice/WWDC/SaveColorTest.swift
@@ -0,0 +1,98 @@
+//
+//  SaveColorTest.swift
+//  SUIPractice
+//
+//  Created by GYURI PARK on 2023/04/13.
+//
+
+import SwiftUI
+
+struct ColorData2 {
+    
+    private var COLOR_KEY = "COLOR_KEY"
+    private let userDefaults = UserDefaults.standard
+    
+    func saveColor(color: Color) {
+        let color = UIColor(color).cgColor
+        
+        if let components = color.components {
+            userDefaults.set(components, forKey: COLOR_KEY)
+            print("이거구나!!!!", components)
+            print("Colour saved!!")
+            
+        }
+    }
+    
+    func loadColor() -> Color {
+        guard let array = userDefaults.object(forKey: COLOR_KEY) as?
+                [CGFloat] else { return Color.white }
+        
+        let color = Color(.sRGB,
+                          red: array[0],
+                          green: array[1],
+                          blue: array[2],
+                          opacity: array[3])
+        print("이걸 넘겨야돼애애애애애애애", color)
+        print("Colour loaded!")
+        
+        return color
+    }
+}
+
+// ==============================================================================
+
+struct SaveColorTest: View {
+    
+    @State private var color: Color = Color.white
+    @State var colorList = [String]()
+    private var colorData = ColorData2()
+    
+    var body: some View {
+        ZStack() {
+            RoundedRectangle(cornerRadius: 50)
+                .foregroundColor(color)
+                .shadow(radius: 10)
+                .frame(width: 300, height: 300)
+                .padding(.top, 50)
+            
+            
+            VStack{
+                ColorPicker("Pick a color", selection: $color)
+                    .labelsHidden()
+                    .padding()
+                
+                Button("Save") {
+                    colorData.saveColor(color: color)
+                    colorList.append(color.description)
+                    print("Test : ", colorData.loadColor())
+                }.foregroundColor(.blue)
+                    .fontWeight(.semibold)
+                    .font(.system(size: 30))
+                    .frame(width: 100)
+                
+            }
+        }
+        .onAppear {
+            color = colorData.loadColor()
+            print("Test color : ", color)
+        }
+    }
+}
+
+struct ColorListView: View {
+    @State var colorList: [String]
+    
+    var body: some View {
+        List(colorList, id: \.self) { color in
+            Text(color)
+        }
+    }
+}
+
+
+struct SaveColorTest_Previews: PreviewProvider {
+    static var previews: some View {
+        SaveColorTest()
+    }
+}
+
